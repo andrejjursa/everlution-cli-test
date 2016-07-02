5d1965db0a5159887caf4e5abc0a71d2154539c3
Andrej Jursa <dragoo@thelambda.sk>
2016-07-02 17:20:02 +0200

    URL content can be fetched now.

--- src/Andrej/Console/JQueryCommand.php
+++ src/Andrej/Console/JQueryCommand.php
@@ -3,6 +3,7 @@
 
 namespace Andrej\Console;
 
+use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 
@@ -11,11 +12,26 @@ class JQueryCommand extends JSScriptFindingCommand {
     protected function configure()
     {
         $this->setName('andrej:jquery');
+        $this->setDescription('This command will test if specified page contains jQuery technology.');
+        $this->addArgument(
+            'url',
+            InputArgument::REQUIRED,
+            'The URL of page where presence of jQuery have to be tested.'
+        );
     }
 
     protected function execute(InputInterface $input, OutputInterface $output)
     {
-        
+        $url = $input->getArgument('url');
+
+        $content = $this->getPageContent($url);
+
+        if (is_string($content)) {
+
+        } else {
+            $output->writeln('<error>Specified URL not found.</error>');
+            return self::COMMON_ERROR_URL_NOT_FOUND;
+        }
     }
 
 }
--- src/Andrej/Console/JSScriptFindingCommand.php
+++ src/Andrej/Console/JSScriptFindingCommand.php
@@ -5,6 +5,29 @@ namespace Andrej\Console;
 
 use Symfony\Component\Console\Command\Command;
 
+/**
+ * Class JSScriptFindingCommand
+ * @package Andrej\Console
+ */
 abstract class JSScriptFindingCommand extends Command {
+
+    const COMMON_ERROR_URL_NOT_FOUND = 100;
+
+    /**
+     * Returns content of page specified by $url.
+     *
+     * @param $url page url to get content from
+     * @param int $timeout timeout for connection, defaults to 5 second if not set
+     * @return string content of page specified by $url
+     */
+    protected function getPageContent($url, $timeout = 5) {
+        $curl_connection = curl_init();
+        curl_setopt($curl_connection, CURLOPT_URL, $url);
+        curl_setopt($curl_connection, CURLOPT_RETURNTRANSFER, 1);
+        curl_setopt($curl_connection, CURLOPT_CONNECTTIMEOUT, $timeout);
+        $content = curl_exec($curl_connection);
+        curl_close($curl_connection);
+        return $content;
+    }
 
 }
