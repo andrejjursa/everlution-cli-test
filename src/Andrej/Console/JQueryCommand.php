42bfd6aadc09ec4fc2abc5d5e16b0cb90fb946b7
Andrej Jursa <dragoo@thelambda.sk>
2016-07-02 18:46:04 +0200

    Command is able to find jquery presence.

--- src/Andrej/Console/JQueryCommand.php
+++ src/Andrej/Console/JQueryCommand.php
@@ -9,6 +9,9 @@ use Symfony\Component\Console\Output\OutputInterface;
 
 class JQueryCommand extends JSScriptFindingCommand {
 
+    const RESPONSE_JQUERY_PRESENT = 0;
+    const RESPONSE_JQUERY_NOT_PRESENT = 1;
+
     protected function configure()
     {
         $this->setName('andrej:jquery');
@@ -27,11 +30,72 @@ class JQueryCommand extends JSScriptFindingCommand {
         $content = $this->getPageContent($url);
 
         if (is_string($content)) {
-
+            $nodes = $this->getHTMLScriptNodes($content);
+            if ($this->findJQueryInNodes($nodes)) {
+                $output->writeln('Specified URL <info>contains</info> jQuery framework.');
+                return self::RESPONSE_JQUERY_PRESENT;
+            } else {
+                $output->writeln('Specified URL <info>do not contains</info> jQuery framework.');
+                return self::RESPONSE_JQUERY_NOT_PRESENT;
+            }
         } else {
             $output->writeln('<error>Specified URL not found.</error>');
             return self::COMMON_ERROR_URL_NOT_FOUND;
         }
     }
+
+    /**
+     * Test if array of identifiers contains jquery identifier.
+     * @param array<string> $identifiers string array of identifiers
+     * @return bool presence of jquery
+     */
+    protected function findJQueryInIdentifiers($identifiers) {
+        if (is_array($identifiers) && !empty($identifiers)) {
+            foreach ($identifiers as $identifier) {
+                if (strtolower($identifier) === 'jquery') {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
+    /**
+     * Test if jquery is present in script src.
+     * @param string $src script source path
+     * @return bool presence of jquery
+     */
+    protected function findJQueryInSrc($src) {
+        if (is_string($src) && mb_strpos(strtolower($src), 'jquery') !== FALSE) {
+            return true;
+        }
+        return false;
+    }
+
+    /**
+     * Test if jquery is present in nodes.
+     * @param array $nodes array of nodes [src, content]
+     * @return bool presence of jquery
+     */
+    protected function findJQueryInNodes($nodes)
+    {
+        if (is_array($nodes) && !empty($nodes)) {
+            foreach ($nodes as $node) {
+                if (!empty($node['src'])) {
+                    if ($this->findJQueryInSrc($node['src'])) {
+                        return true;
+                    }
+                }
+                if (!empty($node['content'])) {
+                    $identifiers = $this->getJavaScriptIdentifier($node['content']);
+                    if ($this->findJQueryInIdentifiers($identifiers)) {
+                        return true;
+                    }
+                }
+            }
+        }
+        return false;
+    }
+
 
 }
--- src/Andrej/Console/JSScriptFindingCommand.php
+++ src/Andrej/Console/JSScriptFindingCommand.php
@@ -4,6 +4,10 @@
 namespace Andrej\Console;
 
 use Symfony\Component\Console\Command\Command;
+use Symfony\Component\DomCrawler\Crawler;
+
+require __DIR__ . '/../../../vendor/timwhitlock/jparser/PLUG/JavaScript/j_token_get_all.php';
+require __DIR__ . '/../../../vendor/timwhitlock/jparser/PLUG/JavaScript/j_token_name.php';
 
 /**
  * Class JSScriptFindingCommand
@@ -25,9 +29,35 @@ abstract class JSScriptFindingCommand extends Command {
         curl_setopt($curl_connection, CURLOPT_URL, $url);
         curl_setopt($curl_connection, CURLOPT_RETURNTRANSFER, 1);
         curl_setopt($curl_connection, CURLOPT_CONNECTTIMEOUT, $timeout);
+        curl_setopt($curl_connection, CURLOPT_FOLLOWLOCATION, true);
         $content = curl_exec($curl_connection);
         curl_close($curl_connection);
         return $content;
+    }
+
+    protected function getHTMLScriptNodes($html_content) {
+        $crawler = new Crawler($html_content);
+
+        $nodes = $crawler->filter('script')->each(function(Crawler $node, $i) {
+            return ['content' => $node->text(), 'src' => $node->attr('src')];
+        });
+
+        return $nodes;
+    }
+
+    protected function getJavaScriptIdentifier($js_code) {
+        $identifiers = [];
+        $identifiers_map = [];
+        $tokens = j_token_get_all($js_code);
+        if (is_array($tokens) && !empty($tokens)) {
+            foreach ($tokens as $token) {
+                if (j_token_name($token[0]) == 'J_IDENTIFIER' && !array_key_exists($token[1], $identifiers_map)) {
+                    $identifiers[] = $token[1];
+                    $identifiers_map[$token[1]] = true;
+                }
+            }
+        }
+        return $identifiers;
     }
 
 }
